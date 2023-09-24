2c0ba21fdc01aef435c73e509571da547ad26f00
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-09-24 05:35:01 -0400

    change

--- components/page/index/category-list.vue
+++ components/page/index/category-list.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="title">Trenddagi turkumlar</div>
+  <div class="category">
+    <div class="title text-center">Trenddagi turkumlar</div>
+   <div class="container">
     <div class="category-list row">
-        <div class="col-6">
+        <div class="col-6" v-for="item of 8">
           <div class="category-box">
             <div class="icon">
-              
+              <img src="@/assets/img/icon-catergory/icon1.png" alt="">
+            </div>
+            <div class="category-body">
+              <div class="category-title">Marketing</div>
+              <div class="category-count">573+ kurslar</div>
             </div>
           </div>
         </div>
     </div>
+   </div>
+  </div>
 </template>
 
 <script>
@@ -18,5 +26,7 @@ export default {
 </script>
 
 <style lang="scss">
-
+.category {
+  padding: 80px 0px 120px;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
 <welcomeVue />
+<categoryList />
 </template>
 
 <script setup>
 import welcomeVue from "~/components/page/index/welcome.vue";
-
+import categoryList from "~/components/page/index/category-list.vue";
 </script>
 
 <style>
