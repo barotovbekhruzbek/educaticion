06d0c8722abb06d6c3b3dad3633ed93b0d663482
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-10-01 17:11:41 +0500

    category list

--- components/page/index/category-list.vue
+++ components/page/index/category-list.vue
@@ -29,4 +29,40 @@ export default {
 .category {
   padding: 80px 0px 120px;
 }
+.category-list {
+  display: flex;
+  gap: 30px;
+}
+.category-box {
+  width: 300px;
+  height: 100px;
+  border-radius: 8px;
+  border: 1px solid var(--border-1, #EDEDED);
+  background: var(--color-5, #FFF);
+  display: flex;
+  gap: 10px;
+  align-items: center;
+
+  .category-body{
+    .category-title {
+      color: var(--Head---Color, #033399);
+      font-size: 17px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 26px;
+      text-transform: capitalize;
+    }
+    .category-count {
+      padding-top: 8px;
+      color: var(--Text---Color, #4F547B);
+
+      font-family: GT Walsheim Pro;
+      font-size: 13px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+  }
+}
+
 </style>
