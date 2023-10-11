140e7fede823e687cc85e1b730bb9302d23f27bb
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-10-11 23:19:29 +0500

    nuxt

--- components/page/index/card-list.vue
+++ components/page/index/card-list.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="cards-list">
     <div class="title text-center">Eng mashhur kurslarimiz</div>
-    <p>10, 000+ soatga ega kurslarimiz</p>
+    <p class="text-center">10, 000+ soatga ega kurslarimiz</p>
+    <Swiper
+    :modules="[SwiperAutoplay, SwiperEffectCreative]"
+    :slides-per-view="3"
+    :space-between="35"
+    :loop="true"
+    :effect="'creative'"
+    :autoplay="{
+      delay: 8000,
+      disableOnInteraction: true,
+    }"
+    :creative-effect="{
+      prev: {
+        shadow: false,
+        translate: ['-20%', 0, -1],
+      },
+      next: {
+        translate: ['100%', 0, 0],
+      },
+    }"
+  >
+    <SwiperSlide v-for="slide in 10" :key="slide">
+      <strong>{{ slide }}</strong>
+    </SwiperSlide>
+  </Swiper>
   </div>
 </template>
 
@@ -11,6 +35,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
 <welcomeVue />
 <categoryList />
+<cardList/>
 </template>
 
 <script setup>
 import welcomeVue from "~/components/page/index/welcome.vue";
 import categoryList from "~/components/page/index/category-list.vue";
+import cardList from "~/components/page/index/card-list.vue";
 </script>
 
 <style>
